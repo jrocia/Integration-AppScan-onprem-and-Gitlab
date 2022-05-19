6677799e07e425136aa9b6c65ef2a408294e0550
jrocia <69405400+jrocia@users.noreply.github.com>
2022-05-19 13:46:56 -0300

    Create appscanstd_check_security_gate.ps1

---
+++ appscanstd_check_security_gate.ps1
@@ -0,0 +1,26 @@
+write-host "======== Step 6 - Checking Security Gate ========"
+"[XML]$xml = Get-Content $CI_PROJECT_DIR\$CI_PROJECT_NAME-$CI_JOB_ID-sevsec.xml"
+$highIssues = $xml.XmlReport.Summary.Hosts.Host.TotalHighSeverityIssues
+$mediumIssues = $xml.XmlReport.Summary.Hosts.Host.TotalMediumSeverityIssues
+$lowIssues = $xml.XmlReport.Summary.Hosts.Host.TotalLowSeverityIssues
+$infoIssues = $xml.XmlReport.Summary.Hosts.Host.TotalInformationalIssues
+$totalIssues = $xml.XmlReport.Summary.Hosts.Host.Total
+write-host "There is $highIssues high issues, $mediumIssues medium issues, $lowIssues low issues and $infoIssues informational issues."
+write-host "The company policy permit less than $maxIssuesAllowed $sevSecGw severity."
+if (( "$highIssues" -gt "$maxIssuesAllowed" ) -and ( "$sevSecGw" -eq "highIssues" )) {
+  write-host "Security Gate build failed"
+  exit 1
+  }
+elseif (( "$mediumIssues" -gt "$maxIssuesAllowed" ) -and ( "$sevSecGw" -eq "mediumIssues" )) {
+  write-host "Security Gate build failed"
+  exit 1
+  }
+elseif (( "$lowIssues" -gt "$maxIssuesAllowed" ) -and ( "$sevSecGw" -eq "lowIssues" )) {
+  write-host "Security Gate build failed"
+  exit 1
+  }
+elseif (( "$totalIssues" -gt "$maxIssuesAllowed" ) -and ( "$sevSecGw" -eq "totalIssues" )) {
+  write-host "Security Gate build failed"
+  exit 1
+  }
+write-host "Security Gate passed"
