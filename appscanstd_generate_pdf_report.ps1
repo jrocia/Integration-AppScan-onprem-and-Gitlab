ed59c52f7152e272b0365990af238d0406918453
jrocia <69405400+jrocia@users.noreply.github.com>
2022-05-19 14:59:12 -0300

    Update appscanstd_generate_pdf_report.ps1

--- appscanstd_generate_pdf_report.ps1
+++ appscanstd_generate_pdf_report.ps1
@@ -1,3 +1,3 @@
 write-host "======== Step 2 - Generating PDF Report ========"
 AppScanCMD.exe /r /b $scanFile /rt pdf /rf $reportPDFFile | out-null
-write-host "======== Step 2 finished ========"
+write-host "Report file $reportPDFFile generated."
