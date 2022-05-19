a4d5e543ddabe8c3cc12b7e3df0929e07f6c5dec
jrocia <69405400+jrocia@users.noreply.github.com>
2022-05-19 15:01:02 -0300

    Update appscanstd_publish_assessment_to_enterprise.ps1

--- appscanstd_publish_assessment_to_enterprise.ps1
+++ appscanstd_publish_assessment_to_enterprise.ps1
@@ -1,3 +1,4 @@
 write-host "======== Step 3 - Publishing Assessment ========"
 AppScanCMD.exe /r /b $scanFile /rt rc_ase /aan $aseAppName > output.txt
+write-host "File $scanFile published on application $aseAppName on AppScan Enterprise." 
 sleep 60;
