5106490381b4930c9c3f5226a163123d6b7098b4
jrocia <69405400+jrocia@users.noreply.github.com>
2022-05-19 13:00:07 -0300

    Update appscanstd_publish_assessment_to_enterprise.ps1

--- appscanstd_publish_assessment_to_enterprise.ps1
+++ appscanstd_publish_assessment_to_enterprise.ps1
@@ -1,6 +1,3 @@
 write-host "======== Step 3 - Publishing Assessment ========"
 AppScanCMD.exe /r /b $scanFile /rt rc_ase /aan $aseAppName > output.txt
-$outputContent=Get-Content .\output.txt
-$scanName=$outputContent.Replace("`0","") | Select-String -Pattern "AppScan Enterprise job '(.*)'" | % {$_.Matches.Groups[1].Value}
-echo "The scan name is $scanName"
 sleep 60;
