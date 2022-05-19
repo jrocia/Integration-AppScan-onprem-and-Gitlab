9ca4bc46329a8d5a58e2d61fe04d0d82286d70b7
jrocia <69405400+jrocia@users.noreply.github.com>
2022-05-19 14:57:07 -0300

    Update appscanstd_scan.ps1

--- appscanstd_scan.ps1
+++ appscanstd_scan.ps1
@@ -7,4 +7,4 @@ else{
   echo "There is a manual explorer file $manualExplore";
   AppScanCMD.exe /su $url /d $scanFile /rt xml /rf $reportXMLsevSec /mef $manualExplore /to
   }
-write-host "======== Step 1 finished ========"
+write-host "Scan on $url finished."
