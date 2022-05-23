ca69b2e72271aba29e836b00efa3ccf233affc53
jrocia <69405400+jrocia@users.noreply.github.com>
2022-05-23 17:50:52 -0300

    Update appscanstd_scan.ps1

--- appscanstd_scan.ps1
+++ appscanstd_scan.ps1
@@ -1,10 +1,17 @@
-write-host "======== Step 1 - Running scan in $url ========"
-if ( [string]::IsNullOrWhiteSpace($manualExplore) ){
-  echo "There is no manual explorer file in $CI_PROJECT_DIR\manualexplore.exd";
-  AppScanCMD.exe /su $url /d $scanFile /rt xml /rf $reportXMLsevSec
+write-host "======== Step: Running scan in $url ========"
+if ([System.IO.File]::Exists($manualExploreDastConfig) -and [System.IO.File]::Exists($loginDastConfig)){
+  write-host "Manual explorer and login file were found in repository folder. It will be used in scanning process."
+  AppScanCMD.exe /su $url /d $scanFile /rt xml /rf $reportXMLsevSec /mef $manualExploreDastConfig /to /lf $loginDastConfig
+  }
+elseif ([System.IO.File]::Exists($manualExploreDastConfig)){
+  write-host "Manual explorer file was found in repository folder. It will be used in scanning process."
+  AppScanCMD.exe /su $url /d $scanFile /rt xml /rf $reportXMLsevSec /mef $manualExploreDastConfig /to
+  }
+elseif ([System.IO.File]::Exists($loginDastConfig)){
+  write-host "Login file was found in repository folder. It will be used in scanning process."
+  AppScanCMD.exe /su $url /d $scanFile /rt xml /rf $reportXMLsevSec /lf $loginDastConfig
   }
 else{
-  echo "There is a manual explorer file $manualExplore";
-  AppScanCMD.exe /su $url /d $scanFile /rt xml /rf $reportXMLsevSec /mef $manualExplore /to
+  AppScanCMD.exe /su $url /d $scanFile /rt xml /rf $reportXMLsevSec
   }
 write-host "Scan on $url finished."
