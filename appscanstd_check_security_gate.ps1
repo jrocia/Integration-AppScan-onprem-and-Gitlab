811441235f83c2aa4fa3d192d05301d3efe125cb
jrocia <69405400+jrocia@users.noreply.github.com>
2022-05-24 14:13:45 -0300

    Update appscanstd_check_security_gate.ps1

--- appscanstd_check_security_gate.ps1
+++ appscanstd_check_security_gate.ps1
@@ -10,20 +10,20 @@ $totalIssues = $xml.XmlReport.Summary.Hosts.Host.Total
 write-host "There is $highIssues high issues, $mediumIssues medium issues, $lowIssues low issues and $infoIssues informational issues."
 write-host "The company policy permit less than $maxIssuesAllowed $sevSecGw severity."
 
-if (( "$highIssues" -gt "$maxIssuesAllowed" ) -and ( "$sevSecGw" -eq "highIssues" )) {
-  write-host "Security Gate build failed"
+if (( $highIssues -gt $maxIssuesAllowed ) -and ( "$sevSecGw" -eq "highIssues" )) {
+  write-host "Security Gate build failed";
   exit 1
   }
-elseif (( "$mediumIssues" -gt "$maxIssuesAllowed" ) -and ( "$sevSecGw" -eq "mediumIssues" )) {
-  write-host "Security Gate build failed"
+elseif (( $mediumIssues -gt $maxIssuesAllowed ) -and ( "$sevSecGw" -eq "mediumIssues" )) {
+  write-host "Security Gate build failed";
   exit 1
   }
-elseif (( "$lowIssues" -gt "$maxIssuesAllowed" ) -and ( "$sevSecGw" -eq "lowIssues" )) {
-  write-host "Security Gate build failed"
+elseif (( $lowIssues -gt $maxIssuesAllowed ) -and ( "$sevSecGw" -eq "lowIssues" )) {
+  write-host "Security Gate build failed";
   exit 1
   }
-elseif (( "$totalIssues" -gt "$maxIssuesAllowed" ) -and ( "$sevSecGw" -eq "totalIssues" )) {
-  write-host "Security Gate build failed"
+elseif (( $totalIssues -gt $maxIssuesAllowed ) -and ( "$sevSecGw" -eq "totalIssues" )) {
+  write-host "Security Gate build failed";
   exit 1
   }
 write-host "Security Gate passed"
