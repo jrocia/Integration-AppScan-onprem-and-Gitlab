c4a2af86b47388f6c07f70630d3866e074341877
jrocia <69405400+jrocia@users.noreply.github.com>
2022-05-24 11:33:48 -0300

    Update appscanase_create_application_ase.ps1

--- appscanase_create_application_ase.ps1
+++ appscanase_create_application_ase.ps1
@@ -7,8 +7,10 @@ if ([string]::IsNullOrWhitespace($aseAppId)){
 	$session.Cookies.Add((New-Object System.Net.Cookie("asc_session_id", "$sessionId", "/", "$aseHostname")));
 	$aseAppId=$(Invoke-WebRequest -Method POST -WebSession $session -Headers @{"Asc_xsrf_token"="$sessionId"} -ContentType "application/json" -Body "{`"name`":`"$aseAppName`" }" -Uri "https://$aseHostname`:9443/ase/api/applications" -SkipCertificateCheck | ConvertFrom-Json).id;
 	echo "$aseAppId" > aseAppId.txt
+	write-host "======== Step: Creating application in AppScan Enterprise ========"
 	write-host "Application $aseAppName registered with id $aseAppId"
     }
 else{
+	write-host "======== Step: Checking application in AppScan Enterprise ========"
 	write-host "There is a registered application."
 	}
