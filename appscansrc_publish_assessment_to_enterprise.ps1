1dc73db5851bca248c7afada03da5b10a3d54a63
jrocia <69405400+jrocia@users.noreply.github.com>
2022-05-25 10:06:38 -0300

    Update appscansrc_publish_assessment_to_enterprise.ps1

--- appscansrc_publish_assessment_to_enterprise.ps1
+++ appscansrc_publish_assessment_to_enterprise.ps1
@@ -1,5 +1,10 @@
 write-host "======== Step: Publishing Assessment in ASE ========"
 
+write-output "login_file $aseHostname $aseToken -acceptssl" > scriptpase.scan
+write-output "RUNAS AUTO" >> scriptpase.scan
+write-output "publishassessase $artifactFolder\$artifactName.ozasmt -aseapplication $aseAppName -name $artifactName-$CI_JOB_ID" >> scriptpase.scan
+write-output "exit" >> scriptpase.scan
 
+AppScanSrcCli scr scriptpase.scan
 
-$artifactFolder\$artifactName.ozasmt
+write-host "Scan published in app "$aseAppName" in ASE"
