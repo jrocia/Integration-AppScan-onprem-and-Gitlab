f7cb1cd5beb0f8577071adcfb9827016e413398f
jrocia <69405400+jrocia@users.noreply.github.com>
2022-05-26 11:35:02 -0300

    Update appscansrc_create_config_scan_file.ps1

--- appscansrc_create_config_scan_file.ps1
+++ appscansrc_create_config_scan_file.ps1
@@ -6,7 +6,6 @@ write-output "oa $artifactFolder\$artifactName -appserver_type Tomcat7 -no_ear_p
 write-output "ra $artifactFolder\$artifactName.ozasmt -scanconfig Normal_scan -name $artifactName-$CI_JOB_ID" >> script.scan
 write-output "report Findings zip $artifactName.zip $artifactFolder\$artifactName.ozasmt -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
 write-output "pa $artifactFolder\$artifactName.ozasmt" >> script.scan
-write-output "publishassessase $artifactFolder\$artifactName.ozasmt -aseapplication $aseAppName -name $artifactName-$CI_JOB_ID" >> script.scan
 write-output "exit" >> script.scan
 
 write-host "Config file created."
