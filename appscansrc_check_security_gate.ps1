c0e8a1730c6b4511e2be50c93fd7d9fdac3ff421
jrocia <69405400+jrocia@users.noreply.github.com>
2022-05-27 20:35:49 -0300

    Update appscansrc_check_security_gate.ps1

--- appscansrc_check_security_gate.ps1
+++ appscansrc_check_security_gate.ps1
@@ -6,22 +6,26 @@ $mediumIssues = $xml.AssessmentRun.AssessmentStats.total_med_finding
 $lowIssues = $xml.AssessmentRun.AssessmentStats.total_low_finding
 $totalIssues = $highIssues+$mediumIssues+$lowIssues
 
-write-host "There is $highIssues high issues, $mediumIssues medium issues and $lowIssues low issues issues."
+write-host "There is $highIssues high issues, $mediumIssues medium issues and $lowIssues low issues."
 write-host "The company policy permit less than $maxIssuesAllowed $sevSecGw severity."
 
 if (( $highIssues -gt $maxIssuesAllowed ) -and ( "$sevSecGw" -eq "highIssues" )) {
+  write-host "High";
   write-host "Security Gate build failed";
   exit 1
   }
 elseif (( $mediumIssues -gt $maxIssuesAllowed ) -and ( "$sevSecGw" -eq "mediumIssues" )) {
+  write-host "Medium";
   write-host "Security Gate build failed";
   exit 1
   }
 elseif (( $lowIssues -gt $maxIssuesAllowed ) -and ( "$sevSecGw" -eq "lowIssues" )) {
+  write-host "Low";
   write-host "Security Gate build failed";
   exit 1
   }
 elseif (( $totalIssues -gt $maxIssuesAllowed ) -and ( "$sevSecGw" -eq "totalIssues" )) {
+  write-host "total";
   write-host "Security Gate build failed";
   exit 1
   }
