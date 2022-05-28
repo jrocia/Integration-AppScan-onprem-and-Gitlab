0bc2d6d13f8b17d0ab485b92d79f6689a254120a
jrocia <69405400+jrocia@users.noreply.github.com>
2022-05-27 22:32:53 -0300

    Update appscanstd_check_security_gate.ps1

--- appscanstd_check_security_gate.ps1
+++ appscanstd_check_security_gate.ps1
@@ -1,11 +1,12 @@
 write-host "======== Step: Checking Security Gate ========"
 
 [XML]$xml = Get-Content *-sevsec.xml
-$highIssues = $xml.XmlReport.Summary.Hosts.Host.TotalHighSeverityIssues
-$mediumIssues = $xml.XmlReport.Summary.Hosts.Host.TotalMediumSeverityIssues
-$lowIssues = $xml.XmlReport.Summary.Hosts.Host.TotalLowSeverityIssues
-$infoIssues = $xml.XmlReport.Summary.Hosts.Host.TotalInformationalIssues
-$totalIssues = $xml.XmlReport.Summary.Hosts.Host.Total
+[int]$highIssues = $xml.XmlReport.Summary.Hosts.Host.TotalHighSeverityIssues
+[int]$mediumIssues = $xml.XmlReport.Summary.Hosts.Host.TotalMediumSeverityIssues
+[int]$lowIssues = $xml.XmlReport.Summary.Hosts.Host.TotalLowSeverityIssues
+[int]$infoIssues = $xml.XmlReport.Summary.Hosts.Host.TotalInformationalIssues
+[int]$totalIssues = $xml.XmlReport.Summary.Hosts.Host.Total
+$maxIssuesAllowed = $maxIssuesAllowed -as [int]
 
 write-host "There is $highIssues high issues, $mediumIssues medium issues, $lowIssues low issues and $infoIssues informational issues."
 write-host "The company policy permit less than $maxIssuesAllowed $sevSecGw severity."
@@ -26,4 +27,6 @@ elseif (( $totalIssues -gt $maxIssuesAllowed ) -and ( "$sevSecGw" -eq "totalIssu
   write-host "Security Gate build failed";
   exit 1
   }
-write-host "Security Gate passed"
+else{
+  write-host "Security Gate passed"
+  }
