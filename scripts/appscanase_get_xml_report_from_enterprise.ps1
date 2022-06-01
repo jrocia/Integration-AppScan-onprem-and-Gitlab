eb7684ceb78c2d0b8f28e1a55a902eb360105f96
jrocia <69405400+jrocia@users.noreply.github.com>
2022-06-01 20:34:33 -0300

    Update appscanase_get_xml_report_from_enterprise.ps1

--- scripts/appscanase_get_xml_report_from_enterprise.ps1
+++ scripts/appscanase_get_xml_report_from_enterprise.ps1
@@ -35,7 +35,7 @@ while ($reportStatusCode -ne 201){
   write-host "Report being generated"
 }
 
-sleep 10;
+sleep 60;
 
 $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession;
 $session.Cookies.Add((New-Object System.Net.Cookie("asc_session_id", "$sessionId", "/", "$aseHostname")));
