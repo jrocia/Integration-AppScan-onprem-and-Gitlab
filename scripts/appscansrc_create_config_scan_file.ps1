2b31bccdcf5cac6227cc1a43e2784d1d1c7d3c04
jrocia <69405400+jrocia@users.noreply.github.com>
2022-07-01 14:19:40 -0300

    Update appscansrc_create_config_scan_file.ps1

--- scripts/appscansrc_create_config_scan_file.ps1
+++ scripts/appscansrc_create_config_scan_file.ps1
@@ -1,5 +1,5 @@
 write-host "======== Step: Creating a config scan file ========"
-
+# Creating Appscan Source script file. It is used with AppScanSrcCli to run scans. It is ready to run scans against java binary files (.jar, .war and .ear). Each language could has a different script. 
 write-output "login_file $aseHostname $aseToken -acceptssl" > script.scan
 write-output "RUNAS AUTO" >> script.scan
 write-output "oa $artifactFolder\$artifactName -appserver_type Tomcat7 -no_ear_project" >> script.scan
