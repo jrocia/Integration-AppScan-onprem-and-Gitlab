0cfb7371335862c7678b11d17e43aaa60e24a64f
jrocia <69405400+jrocia@users.noreply.github.com>
2022-07-01 09:30:53 -0300

    Update appscanase_create_application_ase.ps1

--- scripts/appscanase_create_application_ase.ps1
+++ scripts/appscanase_create_application_ase.ps1
@@ -1,16 +1,18 @@
+write-host "======== Step: Checking application in AppScan Enterprise ========"
+# ASE authentication
 $sessionId=$(Invoke-WebRequest -Method "POST" -Headers @{"Accept"="application/json"} -ContentType 'application/json' -Body "{`"keyId`": `"$aseApiKeyId`",`"keySecret`": `"$aseApiKeySecret`"}" -Uri "https://$aseHostname`:9443/ase/api/keylogin/apikeylogin" -SkipCertificateCheck | Select-Object -Expand Content | ConvertFrom-Json | select -ExpandProperty sessionId);
+# Looking for $aseAppName into ASE
 $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession;
 $session.Cookies.Add((New-Object System.Net.Cookie("asc_session_id", "$sessionId", "/", "$aseHostname")));
 $aseAppId=$(Invoke-WebRequest -WebSession $session -Headers @{"Asc_xsrf_token"="$sessionId"} -Uri "https://$aseHostname`:9443/ase/api/applications/search?searchTerm=$aseAppName" -SkipCertificateCheck | ConvertFrom-Json).id;
+# If $aseAppName is Null create the application into ASE else just get the aseAppId
 if ([string]::IsNullOrWhitespace($aseAppId)){
 	$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession;
 	$session.Cookies.Add((New-Object System.Net.Cookie("asc_session_id", "$sessionId", "/", "$aseHostname")));
 	$aseAppId=$(Invoke-WebRequest -Method POST -WebSession $session -Headers @{"Asc_xsrf_token"="$sessionId"} -ContentType "application/json" -Body "{`"name`":`"$aseAppName`" }" -Uri "https://$aseHostname`:9443/ase/api/applications" -SkipCertificateCheck | ConvertFrom-Json).id;
 	echo "$aseAppId" > aseAppId.txt
-	write-host "======== Step: Creating application in AppScan Enterprise ========"
 	write-host "Application $aseAppName registered with id $aseAppId"
     }
 else{
-	write-host "======== Step: Checking application in AppScan Enterprise ========"
 	write-host "There is a registered application."
 	}
