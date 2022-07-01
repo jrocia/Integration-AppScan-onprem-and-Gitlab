d00753edef1c0a60076cbb26abff4037f2d26deb
jrocia <69405400+jrocia@users.noreply.github.com>
2022-07-01 14:29:09 -0300

    Update appscanstd_generate_pdf_report.ps1

--- scripts/appscanstd_generate_pdf_report.ps1
+++ scripts/appscanstd_generate_pdf_report.ps1
@@ -1,3 +1,4 @@
 write-host "======== Step: Generating PDF Report ========"
+# Executing AppScan Standard to generate a PDF report based in a Scan File (.scan)
 AppScanCMD.exe /r /b $scanFile /rt pdf /rf $reportPDFFile | out-null
 write-host "Report file $reportPDFFile generated."
