4a0a7737498390e180bf9428249dfdff8a81d23f
jrocia <69405400+jrocia@users.noreply.github.com>
2022-09-12 19:46:29 -0300

    Update appscanase_create_folder_ase.ps1

--- scripts/appscanase_create_folder_ase.ps1
+++ scripts/appscanase_create_folder_ase.ps1
@@ -4,16 +4,16 @@ $sessionId=$(Invoke-WebRequest -Method "POST" -Headers @{"Accept"="application/j
 # Looking for $aseAppName into ASE
 $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession;
 $session.Cookies.Add((New-Object System.Net.Cookie("asc_session_id", "$sessionId", "/", "$aseHostname")));
-#  Looking for $aseAppFolderId into ASE
 $aseAppFolderId=$(Invoke-WebRequest -WebSession $session -Headers @{"Asc_xsrf_token"="$sessionId"} -Uri "https://$aseHostname`:9443/ase/api/folders/search?searchString=$aseAppName" -SkipCertificateCheck | ConvertFrom-Json).folderId;
 # If $aseAppFolderId is Null create the application folder into ASE else just get the aseAppFolderId
 if ([string]::IsNullOrWhitespace($aseAppFolderId)){
 	$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession;
 	$session.Cookies.Add((New-Object System.Net.Cookie("asc_session_id", "$sessionId", "/", "$aseHostname")));
-	[XML]$aseAppFolder=$(Invoke-WebRequest -Method POST -WebSession $session -Headers @{"Asc_xsrf_token"="$sessionId"} -ContentType "application/json" -Body "{`"parentId`":1,`"folderName`":`"$aseAppName`",`"description`":`"`",`"contact`":0}" -Uri "https://$aseHostname`:9443/ase/api/folders/create" -SkipCertificateCheck);
+	[XML]$aseAppFolder=$(Invoke-WebRequest -Method POST -WebSession $session -Headers @{"Asc_xsrf_token"="$sessionId";"Accept"="application/xml"} -ContentType "application/json" -Body "{`"parentId`":1,`"folderName`":`"$aseAppName`",`"description`":`"`",`"contact`":0}" -Uri "https://$aseHostname`:9443/ase/api/folders/create" -SkipCertificateCheck).content.Replace('ï»¿','');
 	write-output $aseAppFolder.folder.id > aseAppFolderId.txt;
+	$aseAppFolderId=Get-Content .\aseAppFolderId.txt
 	write-host "aseAppFolderId $aseAppFolderId created for Application $aseAppName.";
     }
 else{
-	write-host "There is a aseAppFolderId $aseAppFolderId created."
+	write-host "There is a aseAppFolderId $aseAppFolderId created.";
 	}
