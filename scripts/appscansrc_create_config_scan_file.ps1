23630f7c2588e0db6a8ca299f123ea58776ba757
jrocia <69405400+jrocia@users.noreply.github.com>
2023-01-02 16:05:29 -0300

    Update appscansrc_create_config_scan_file.ps1

--- scripts/appscansrc_create_config_scan_file.ps1
+++ scripts/appscansrc_create_config_scan_file.ps1
@@ -3,7 +3,7 @@ write-host "======== Step: Creating a config scan file ========"
 write-output "login_file $aseHostname $aseToken -acceptssl" > script.scan
 write-output "RUNAS AUTO" >> script.scan
 write-output "oa $artifactFolder\$artifactName -appserver_type Tomcat7 -no_ear_project" >> script.scan
-write-output "ra $artifactFolder\$artifactName.ozasmt -scanconfig Normal_scan -name $artifactName-$CI_JOB_ID" >> script.scan
+write-output "ra $artifactFolder\$artifactName.ozasmt -scanconfig "Normal scan" -name $artifactName-$CI_JOB_ID" >> script.scan
 write-output "report Findings zip $artifactName.zip $artifactFolder\$artifactName.ozasmt -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
 write-output "pa $artifactFolder\$artifactName.ozasmt" >> script.scan
 write-output "exit" >> script.scan
