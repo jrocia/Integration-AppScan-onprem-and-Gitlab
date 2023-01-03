486bb53bc5aab9ddd5f0ac8e8cf5fc5daccdbf43
jrocia <69405400+jrocia@users.noreply.github.com>
2023-01-03 16:21:09 -0300

    Update appscansrc_create_config_scan_folder.ps1

--- scripts/appscansrc_create_config_scan_folder.ps1
+++ scripts/appscansrc_create_config_scan_folder.ps1
@@ -3,7 +3,7 @@ write-host "======== Step: Creating a config scan folder ========"
 write-output "login_file $aseHostname $aseToken -acceptssl" > script.scan
 write-output "RUNAS AUTO" >> script.scan
 write-output "of $artifactFolder" >> script.scan
-write-output "sc $artifactFolder\$artifactName.ozasmt -scanconfig "Normal scan" -name $artifactName-$CI_JOB_ID" >> script.scan
+write-output "sc $artifactFolder\$artifactName.ozasmt -scanconfig Normal scan -name $artifactName-$CI_JOB_ID" >> script.scan
 write-output "report Findings zip $artifactName.zip $artifactFolder\$artifactName.ozasmt -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
 write-output "pa $artifactFolder\$artifactName.ozasmt" >> script.scan
 write-output "exit" >> script.scan
