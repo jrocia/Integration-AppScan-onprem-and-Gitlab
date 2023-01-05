702592f9a59de943657e889c025bb7c4142b2779
jrocia <69405400+jrocia@users.noreply.github.com>
2023-01-05 15:00:47 -0300

    Update appscansrc_scan.ps1

--- scripts/appscansrc_scan.ps1
+++ scripts/appscansrc_scan.ps1
@@ -1,4 +1,4 @@
 write-host "======== Step: Running scan in $artifactName ========"
 # Running AppScan Source scan through AppScanSrcCli
 AppScanSrcCli scr script.scan
-copy $artifactFolder\$artifactName.ozasmt .
+# copy $artifactFolder\$artifactName.ozasmt .
