41cc44958323247b98c46f73294fc37180b31b0f
jrocia <69405400+jrocia@users.noreply.github.com>
2023-04-13 16:20:46 -0300

    Update appscansrc_publish_assessment_to_enterprise_demo.ps1

--- scripts/appscansrc_publish_assessment_to_enterprise_demo.ps1
+++ scripts/appscansrc_publish_assessment_to_enterprise_demo.ps1
@@ -2,11 +2,11 @@ write-host "======== Step: Publishing Assessment in ASE ========"
 # Creating script to get ozasmt scan result
 write-output "login" > scriptpase.scan
 write-output "RUNAS AUTO" >> scriptpase.scan
-write-output "publishassessase $CI_PROJECT_DIR-$CI_JOB_ID.ozasmt -aseapplication $aseAppName -name $CI_PROJECT_DIR-$CI_JOB_ID" >> scriptpase.scan
+write-output "publishassessase $CI_PROJECT_NAME-$CI_JOB_ID.ozasmt -aseapplication $aseAppName -name $CI_PROJECT_NAME-$CI_JOB_ID" >> scriptpase.scan
 write-output "exit" >> scriptpase.scan
 # Executing the script
 AppScanSrcCli scr scriptpase.scan
 # Getting and writing the scanName in a file
-$scanName="$CI_PROJECT_DIR-$CI_JOB_ID"
+$scanName="$CI_PROJECT_NAME-$CI_JOB_ID"
 write-output $scanName > scanName_var.txt
 write-host "The scan $scanName was published in app $aseAppName in ASE"
