41a6f40e101548b00c27c315a8c509172891d450
jrocia <69405400+jrocia@users.noreply.github.com>
2023-04-13 17:36:30 -0300

    Update appscansrc_create_config_scan_folder_demo.ps1

--- scripts/appscansrc_create_config_scan_folder_demo.ps1
+++ scripts/appscansrc_create_config_scan_folder_demo.ps1
@@ -2,7 +2,7 @@ write-host "======== Step: Creating a config scan folder ========"
 # Creating Appscan Source script file. It is used with AppScanSrcCli to run scans reading folder content and selecting automatically the language (Open Folder command). 
 write-output "login" > script.scan
 write-output "RUNAS AUTO" >> script.scan
-write-output "of `".\`"" >> script.scan
+write-output "of `"$CI_PROJECT_DIR`"" >> script.scan
 write-output "sc `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -scanconfig `"Normal scan`" -name `"$CI_PROJECT_NAME-$CI_JOB_ID`" -sourcecodeonly true" >> script.scan
 write-output "report Findings pdf `"$CI_PROJECT_NAME-$CI_JOB_ID.pdf`" `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
 # write-output "pa `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`"" >> script.scan
