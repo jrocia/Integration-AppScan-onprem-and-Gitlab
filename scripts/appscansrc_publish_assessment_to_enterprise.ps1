a470a14e7dd5d6e399dba0a76985bf25c60cf38d
jrocia <69405400+jrocia@users.noreply.github.com>
2023-04-14 22:29:15 -0300

    Update appscansrc_publish_assessment_to_enterprise.ps1

--- scripts/appscansrc_publish_assessment_to_enterprise.ps1
+++ scripts/appscansrc_publish_assessment_to_enterprise.ps1
@@ -3,13 +3,13 @@ write-host "======== Step: Publishing Assessment in ASE ========"
 if ($compiledArtifactFolder -ne "none"){
   write-output "login_file $aseHostname $aseToken -acceptssl" > scriptpase.scan
   write-output "RUNAS AUTO" >> scriptpase.scan
-  write-output "publishassessase $CI_PROJECT_DIR\$compiledArtifactFolder\$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt -aseapplication $aseAppName -name $CI_PROJECT_NAME-$CI_JOB_ID" >> scriptpase.scan
+  write-output "pase $CI_PROJECT_DIR\$compiledArtifactFolder\$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt -aseapplication $aseAppName -name $CI_PROJECT_NAME-$CI_JOB_ID" >> scriptpase.scan
   write-output "exit" >> scriptpase.scan
 }
 else{
   write-output "login_file $aseHostname $aseToken -acceptssl" > scriptpase.scan
   write-output "RUNAS AUTO" >> scriptpase.scan
-  write-output "publishassessase $CI_PROJECT_DIR\$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt -aseapplication $aseAppName -name $CI_PROJECT_NAME-$CI_JOB_ID" >> scriptpase.scan
+  write-output "pase $CI_PROJECT_DIR\$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt -aseapplication $aseAppName -name $CI_PROJECT_NAME-$CI_JOB_ID" >> scriptpase.scan
   write-output "exit" >> scriptpase.scan  
 }
 
