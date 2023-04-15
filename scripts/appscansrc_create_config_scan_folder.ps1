cf04c36a9e3ab393134ddefa9116937e6d8c446c
jrocia <69405400+jrocia@users.noreply.github.com>
2023-04-14 22:28:32 -0300

    Update appscansrc_create_config_scan_folder.ps1

--- scripts/appscansrc_create_config_scan_folder.ps1
+++ scripts/appscansrc_create_config_scan_folder.ps1
@@ -5,7 +5,7 @@ if ($compiledArtifactFolder -ne "none"){
   write-output "RUNAS AUTO" >> script.scan
   write-output "of `"$CI_PROJECT_DIR\$compiledArtifactFolder`"" >> script.scan
   write-output "sc `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -scanconfig `"Normal scan`" -name `"$CI_PROJECT_NAME-$CI_JOB_ID`"" >> script.scan
-  write-output "report Findings pdf-detailed `"$CI_PROJECT_NAME-$CI_JOB_ID.pdf`" `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
+  write-output "report Findings pdf-detailed `"$CI_PROJECT_NAME-$CI_JOB_ID.pdf`" `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect" >> script.scan
   write-output "pa `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`"" >> script.scan
   write-output "exit" >> script.scan
   
@@ -16,7 +16,7 @@ else{
   write-output "RUNAS AUTO" >> script.scan
   write-output "of `"$CI_PROJECT_DIR`"" >> script.scan
   write-output "sc `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -scanconfig `"Normal scan`" -name `"$CI_PROJECT_NAME-$CI_JOB_ID`" -sourcecodeonly true" >> script.scan
-  write-output "report Findings pdf-detailed `"$CI_PROJECT_NAME-$CI_JOB_ID.pdf`" `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
+  write-output "report Findings pdf-detailed `"$CI_PROJECT_NAME-$CI_JOB_ID.pdf`" `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect" >> script.scan
   write-output "pa `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`"" >> script.scan
   write-output "exit" >> script.scan
   
