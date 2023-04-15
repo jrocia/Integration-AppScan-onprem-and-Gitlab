e8ec315da1bef6c6ecd7e59ff20a615805f07129
jrocia <69405400+jrocia@users.noreply.github.com>
2023-04-14 23:26:10 -0300

    Update appscansrc_publish_assessment_to_enterprise.ps1

--- scripts/appscansrc_publish_assessment_to_enterprise.ps1
+++ scripts/appscansrc_publish_assessment_to_enterprise.ps1
@@ -1,17 +1,8 @@
 write-host "======== Step: Publishing Assessment in ASE ========"
 # Creating script to get ozasmt scan result
-if ($compiledArtifactFolder -ne "none"){
-  write-output "login_file $aseHostname $aseToken -acceptssl" > scriptpase.scan
-  #write-output "RUNAS AUTO" >> scriptpase.scan
-  write-output "pase $CI_PROJECT_DIR\$compiledArtifactFolder\$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt -aseapplication $aseAppName -name $CI_PROJECT_NAME-$CI_JOB_ID" >> scriptpase.scan
-  write-output "exit" >> scriptpase.scan
-}
-else{
-  write-output "login_file $aseHostname $aseToken -acceptssl" > scriptpase.scan
-  #write-output "RUNAS AUTO" >> scriptpase.scan
-  write-output "pase $CI_PROJECT_DIR\$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt -aseapplication $aseAppName -name $CI_PROJECT_NAME-$CI_JOB_ID" >> scriptpase.scan
-  write-output "exit" >> scriptpase.scan  
-}
+write-output "login_file $aseHostname $aseToken -acceptssl" > scriptpase.scan
+write-output "pase $CI_PROJECT_DIR\$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt -aseapplication $aseAppName -name $CI_PROJECT_NAME-$CI_JOB_ID" >> scriptpase.scan
+write-output "exit" >> scriptpase.scan  
 
 # Executing the script
 AppScanSrcCli scr scriptpase.scan
