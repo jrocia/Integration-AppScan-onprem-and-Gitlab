671ce0eabae5866474d8d6d1692ae298af247e29
jrocia <69405400+jrocia@users.noreply.github.com>
2023-04-14 22:00:43 -0300

    Update appscansrc_create_config_scan_folder.ps1

--- scripts/appscansrc_create_config_scan_folder.ps1
+++ scripts/appscansrc_create_config_scan_folder.ps1
@@ -1,11 +1,24 @@
 write-host "======== Step: Creating a config scan folder ========"
-# Creating Appscan Source script file. It is used with AppScanSrcCli to run scans reading folder content and selecting automatically the language (Open Folder command). 
-write-output "login_file $aseHostname `"$aseToken`" -acceptssl" > script.scan
-write-output "RUNAS AUTO" >> script.scan
-write-output "of `"$artifactFolder`"" >> script.scan
-write-output "sc `"$artifactFolder\$artifactName.ozasmt`" -scanconfig `"Normal scan`" -name `"$artifactName-$CI_JOB_ID`"" >> script.scan
-write-output "report Findings zip `"$artifactName.zip`" `"$artifactFolder\$artifactName.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
-write-output "pa `"$artifactFolder\$artifactName.ozasmt`"" >> script.scan
-write-output "exit" >> script.scan
-
-write-host "Config file created."
+# Creating Appscan Source script file. It is used with AppScanSrcCli to run scans reading folder content and selecting automatically the language (Open Folder command).
+if ($compiledArtifactFolder -ne "none"){
+  write-output "login_file $aseHostname `"$aseToken`" -acceptssl" > script.scan
+  write-output "RUNAS AUTO" >> script.scan
+  write-output "of `"$CI_PROJECT_DIR\$compiledArtifactFolder`"" >> script.scan
+  write-output "sc `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -scanconfig `"Normal scan`" -name `"$CI_PROJECT_NAME-$CI_JOB_ID`"" >> script.scan
+  write-output "report Findings pdf-detailed `"$CI_PROJECT_NAME-$CI_JOB_ID.pdf`" `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
+  write-output "pa `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`"" >> script.scan
+  write-output "exit" >> script.scan
+  
+  write-host "Config file created for compiled folder ($CI_PROJECT_DIR\$compiledArtifactFolder)."
+}
+else{
+  write-output "login_file $aseHostname `"$aseToken`" -acceptssl" > script.scan
+  write-output "RUNAS AUTO" >> script.scan
+  write-output "of `"$CI_PROJECT_DIR`"" >> script.scan
+  write-output "sc `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -scanconfig `"Normal scan`" -name `"$CI_PROJECT_NAME-$CI_JOB_ID`" -sourcecodeonly true" >> script.scan
+  write-output "report Findings pdf-detailed `"$CI_PROJECT_NAME-$CI_JOB_ID.pdf`" `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect" -includeHowToFix" >> script.scan
+  write-output "pa `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`"" >> script.scan
+  write-output "exit" >> script.scan
+  
+  write-host "Config file created."
+}
