07b91e68605f33967acf0446b72295271ca1756f
jrocia <69405400+jrocia@users.noreply.github.com>
2023-04-17 21:45:21 -0300

    Update appscansrc_create_config_scan_folder_demo.ps1

--- scripts/appscansrc_create_config_scan_folder_demo.ps1
+++ scripts/appscansrc_create_config_scan_folder_demo.ps1
@@ -18,8 +18,7 @@ write-output "login_file $aseHostname `"$aseToken`" -acceptssl" > script.scan
 write-output "RUNAS AUTO" >> script.scan
 write-output "of `"$CI_PROJECT_DIR`"" >> script.scan
 write-output "sc `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -scanconfig `"Normal scan`" -name `"$CI_PROJECT_NAME-$CI_JOB_ID`" -sourcecodeonly true" >> script.scan
-#write-output "report Findings pdf-detailed `"$CI_PROJECT_NAME-$CI_JOB_ID.pdf`" `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
-write-output "report Findings pdf-detailed `"$CI_PROJECT_NAME-$CI_JOB_ID.pdf`" `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect" >> script.scan
+write-output "report Findings pdf-detailed `"$CI_PROJECT_NAME-$CI_JOB_ID.pdf`" `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
 write-output "pa `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`"" >> script.scan
 write-output "exit" >> script.scan
 
