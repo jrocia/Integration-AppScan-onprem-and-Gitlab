be0cb9977981ad92639670e850a452d2ecd48a30
jrocia <69405400+jrocia@users.noreply.github.com>
2023-04-26 10:54:15 -0300

    Update appscansrc_publish_assessment_to_enterprise.ps1

--- scripts/appscansrc_publish_assessment_to_enterprise.ps1
+++ scripts/appscansrc_publish_assessment_to_enterprise.ps1
@@ -15,12 +15,12 @@
 write-host "======== Step: Publishing Assessment in ASE ========"
 # Creating script to get ozasmt scan result
 write-output "login_file $aseHostname $aseToken -acceptssl" > scriptpase.scan
-write-output "pase $CI_PROJECT_DIR\$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt -aseapplication $aseAppName -name $CI_PROJECT_NAME-$CI_JOB_ID" >> scriptpase.scan
+write-output "pase $CI_PROJECT_DIR\$aseAppName-$CI_JOB_ID.ozasmt -aseapplication $aseAppName -name $aseAppName-$CI_JOB_ID" >> scriptpase.scan
 write-output "exit" >> scriptpase.scan  
 
 # Executing the script
 AppScanSrcCli scr scriptpase.scan
 # Getting and writing the scanName in a file
-$scanName="$CI_PROJECT_NAME`-$CI_JOB_ID"
+$scanName="$aseAppName`-$CI_JOB_ID"
 write-output $scanName > scanName_var.txt
 write-host "The scan $scanName was published in app $aseAppName in ASE"
